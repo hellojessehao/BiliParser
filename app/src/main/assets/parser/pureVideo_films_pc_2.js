961bb80019bc7de2d63c27acc522e14eb4b4bb44
hellojessehao <316778169@qq.com>
2020-04-23 15:53:32 +0800

    1.解决影视剧播放时换集会重定向回第一集问题； 2.彻底去除播放页面的广告和无用元素，只保留视频； 3.版本号升级到1.1.2。

--- app/src/main/assets/parser/pureVideo_films_pc_2.js
+++ app/src/main/assets/parser/pureVideo_films_pc_2.js
@@ -14,12 +14,15 @@ var head = document.getElementsByTagName('head')[0];
 var tb_player = document.getElementsByClassName('tb player')[0];
 var player_box = document.getElementsByClassName('player-box')[0];
 var player_ff = document.getElementsByClassName('player-ff')[0];
+var macPlayer = document.getElementsByClassName('MacPlayer')[0];
 for(var i=0;i<arr.length;i++){
     var tagName = arr[i].tagName;
     var id = arr[i].id;
     var className = arr[i].className;
     if(tagName.indexOf('HTML') != -1 || tagName.indexOf('BODY') != -1
     || (tagName.indexOf('HEAD') != -1 && tagName.indexOf("HEADER") == -1)
+    || tagName.indexOf('TBODY') != -1 || tagName.indexOf('TR') != -1
+    || id.indexOf('playleft') != -1 || tagName.indexOf('IFRAME') != -1
     || className.indexOf('player-box') != -1
     || className.indexOf('player-ff') != -1
     || className.indexOf('tb player') != -1
@@ -27,12 +30,53 @@ for(var i=0;i<arr.length;i++){
     || arr[i].parentNode == tb_player
     || arr[i].parentNode == player_box
     || arr[i].parentNode == player_ff
+    || arr[i].parentNode == macPlayer
     ){
-        window.customScript.log(' tagName = '+tagName+" ,id = "+id);
         continue;
     }
     arr[i].parentNode.removeChild(arr[i]);
 }
+var tab_r = document.getElementsByClassName('tab-r')[0];
+if(tab_r){
+    tab_r.parentNode.removeChild(tab_r);
+}
+var player_info = document.getElementsByClassName('player-info')[0];
+if(player_info){
+    player_info.parentNode.removeChild(player_info);
+}
+var els_sharebox = document.getElementsByClassName('els-sharebox')[0];
+if(els_sharebox){
+    els_sharebox.parentNode.removeChild(els_sharebox);
+}
+var player_vinfo = document.getElementsByClassName('player-vinfo')[0];
+if(player_vinfo){
+    player_vinfo.parentNode.removeChild(player_vinfo);
+}
+var playopen = document.getElementsByClassName('playopen')[0];
+if(playopen){
+    playopen.parentNode.removeChild(playopen);
+}
+
+var body = document.getElementsByTagName('body')[0];
+if(tb_player){
+    tb_player.className = '';
+    tb_player.id = '';
+}
+if(player_box){
+    player_box.className = '';
+    player_box.id = '';
+}
+if(player_ff){
+    player_ff.className = '';
+    player_ff.id = '';
+}
+if(macPlayer){
+    macPlayer.style.height = '1000px';
+}
+var iframeArr = document.getElementsByTagName('iframe');
+iframeArr[0].parentNode.removeChild(iframeArr[0]);
+iframeArr[0].style.height = '1000px';
+
 var html = document.getElementsByTagName('html')[0];
 window.customScript.log('html : '+html.innerHTML);
 window.customScript.onJSLoadComplete();
